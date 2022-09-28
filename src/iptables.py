47880b209fd8c5c97f074c899c10ac86fb43c2b9
RustyC0der <miller_maers@tutanota.com>
2022-09-28 16:04:22 +0300

    add docs

--- src/iptables.py
+++ src/iptables.py
@@ -3,30 +3,41 @@ from abstract.abstractBlock import BanMethod
 import iptc
 
 class IptablesBlockMethod(BanMethod):
-
+    """
+    class that implemets iptables ip blocking logic
+    """
     custom_chain_name = 'nginx-ddos-protection-chain'
 
-    # check or create chain
+    
     def __init__(self):
-        # use table filter
+        """
+        on init we createing new empty chain inside "INPUT" table to hold all blocked users separately
+        then we redirect all connections to custom chain
+        """
         table = iptc.Table(iptc.Table.FILTER)
 
         # return if chain exists
         for chain in table.chains:
             if chain.name == self.custom_chain_name:
                 self.custom_chain = chain
-                print("chain exists")
                 return
         
         # create chain and add to chain input
         self.custom_chain = table.create_chain(self.custom_chain_name)
+        custom_chain_return_rule = iptc.Rule()
+        custom_chain_return_rule.create_target("RETURN")
+        self.custom_chain.append_rule(custom_chain_return_rule)
+
         input_chain = iptc.Chain(table, "INPUT")
-        custom_chain_rule = iptc.Rule()
-        custom_chain_rule.create_target(self.custom_chain_name)
-        input_chain.insert_rule(custom_chain_rule)
+        custom_chain_redirect_rule = iptc.Rule()
+        custom_chain_redirect_rule.create_target(self.custom_chain_name)
+        input_chain.insert_rule(custom_chain_redirect_rule)
 
 
     def ban(self, ip):
+        """
+        add drop rule with *ip*
+        """
         rule = iptc.Rule()
         rule.src = ip
         rule.create_target("DROP")
@@ -35,6 +46,9 @@ class IptablesBlockMethod(BanMethod):
 
 
     def unban(self, ip):
+        """
+        delete drop rule with *ip*
+        """
         rule = iptc.Rule()
         rule.src = ip
         rule.create_target("DROP")
@@ -42,9 +56,15 @@ class IptablesBlockMethod(BanMethod):
         self.custom_chain.delete_rule(rule)
 
     def flush(self):
+        """
+        clean blocked users chain
+        """
         self.custom_chain.flush()
     
     def cleanup(self):
+        """
+        move all chains and rules created by class
+        """
         input_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
         custom_chain_rule = iptc.Rule()
         custom_chain_rule.create_target(self.custom_chain_name)
@@ -53,6 +73,5 @@ class IptablesBlockMethod(BanMethod):
         self.flush()
         self.custom_chain.delete()
 
-a = IptablesBlockMethod()
-a.ban("192.168.1.2")
-a.cleanup()
+if __name__ == "__main__":
+    a = IptablesBlockMethod()
