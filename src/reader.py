74026e31071b9cd52442cf3d614b24d6d249c4f6
RustyC0der <miller_maers@tutanota.com>
2022-09-28 21:14:51 +0300

    add doc

--- src/reader.py
+++ src/reader.py
@@ -1,8 +1,14 @@
-class BatchReader():
+class SplitReader():
+    """
+    line by line reader
+    it reads file then splits with separator and returns tuple
 
+    """
 
+    # read from end of file
+    last_lines_to_read_count = 100000
+    separator = "/"
 
-    CHUNK_SIZE = 8092
-
-    def __init__(self, target):
+    def __init__(self, target_file, read_from_begin=False):
         pass
+
