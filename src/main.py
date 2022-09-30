d88674d2921e5b2ac361fb496934f3d98515fc0e
RustyC0der <miller_maers@tutanota.com>
2022-09-30 18:32:11 +0300

    upd

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 from iptables import IptablesBlockMethod
-from reader import BatchReader
+from reader import ChunkReader
+from parser import SplitParser
 
-
-logfile = "/dev/shm/access.log"
+logfile = "nginx_test_conf/log/access.log"
 
 
 banner = IptablesBlockMethod()
-reader = BatchReader(logfile)
+reader = ChunkReader(logfile)
+parser = SplitParser(reader)
 
-datastore = {}
+frequency_datastore = {}
+ratelimit = 1000 # requests per minute
 
 for ip in reader:
     pass
