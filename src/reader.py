b677f22dbfc31a157266f75c7535c0b085ef80ee
RustyC0der <miller_maers@tutanota.com>
2022-09-30 14:06:46 +0300

    rewtite non-optimal tail algorithm

--- src/reader.py
+++ src/reader.py
@@ -1,35 +1,44 @@
-from time import sleep
+from time import sleep, time
 import os
+
 
 class SplitReader():
     """
     line by line reader
     it reads file then splits with separator and returns tuple
-
     """
 
     # read from end of file
-    last_lines_to_read_count = 100000
+    last_bytes_to_read_count = 100000
     separator = "/"
 
     def __init__(self, target_file, read_from_begin=False):
-        pass
+        if os.path.isfile(target_file) and os.access(target_file, os.R_OK):
+            self.file = target_file
+        else:
+            raise IOError("file not exist or not readable")
+        
 
 
-    def tail(f, lines=15, _buffer=4098):
+    def _tail(self, lines=15000, _buffer=4096):
         """Tail a file and get X lines from the end"""
-        lines_found = []
+        with open(self.file, 'rt') as f:
+            lines_found = []
+            f.seek(0, 2)
+            position = f.tell() - _buffer
+            while (len(lines_found) < lines) and position >= 0:
+                f.seek(position)
+                got_lines = f.read(_buffer).split('\n')[1:-1]
+                lines_found = got_lines + lines_found
+                
+                position -= _buffer
 
-        block_counter = -1
-        while len(lines_found) < lines:
-            try:
-                f.seek(block_counter * _buffer, os.SEEK_END)
-            except IOError:  # either file is too small, or too many lines requested
-                f.seek(0)
-                lines_found = f.readlines()
-                break
+            return lines_found[-lines:]
 
-            lines_found = f.readlines()
-            block_counter -= 1
 
-        return lines_found[-lines:]
+
+if __name__ == '__main__':
+    rd = SplitReader("nginx_test_conf/log/access.log")
+    t1 = time()
+    print(rd._tail())
+    print (time() - t1)
