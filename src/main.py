b7c531457d58bd18af92c03b591aa8055ca9f82b
RustyC0der <miller_maers@tutanota.com>
2022-09-30 21:59:21 +0300

    testing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from time import time, sleep
 from iptables import IptablesBlockMethod
 from reader import ChunkReader
 from parser import SplitParser
@@ -5,12 +6,31 @@ from parser import SplitParser
 logfile = "nginx_test_conf/log/access.log"
 
 
-banner = IptablesBlockMethod()
+# banner = IptablesBlockMethod()
 reader = ChunkReader(logfile)
 parser = SplitParser(reader)
 
 frequency_datastore = {}
-ratelimit = 1000 # requests per minute
 
-for ip in reader:
-    pass
+period_seconds = 60
+ratelimit = 1000
+sleep_seconds = 1
+
+
+def filter_ips(frequency_datastore, treshold):
+    for ip in frequency_datastore:
+        if frequency_datastore[ip] > treshold:
+            pass
+            # ban ip
+
+time_now = time()
+for ip in parser:
+
+    if not ip:
+        sleep(sleep_seconds)
+        continue
+
+    try:
+        frequency_datastore[ip] += 1 
+    except KeyError:
+        frequency_datastore[ip] = 1
