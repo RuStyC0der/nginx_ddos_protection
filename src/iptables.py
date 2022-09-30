a9218834b46d3486a83ee7e79ef1edc2c0371293
RustyC0der <miller_maers@tutanota.com>
2022-09-30 15:18:34 +0300

    add datasorce to make ban and unban idempotent

--- src/iptables.py
+++ src/iptables.py
@@ -7,8 +7,8 @@ class IptablesBlockMethod(BanMethod):
     class that implemets iptables ip blocking logic
     """
     custom_chain_name = 'nginx-ddos-protection-chain'
+    ip_datastore = set()
 
-    
     def __init__(self):
         """
         on init we createing new empty chain inside "INPUT" table to hold all blocked users separately
@@ -17,10 +17,11 @@ class IptablesBlockMethod(BanMethod):
         """
         table = iptc.Table(iptc.Table.FILTER)
 
-        # do nothing if chain exists
+        # if chain exists bound it and load ips from it
         for chain in table.chains:
             if chain.name == self.custom_chain_name:
                 self.custom_chain = chain
+                self.export_chain_rules_to_datastore()
                 return
         
         # create and configure chain
@@ -36,26 +37,47 @@ class IptablesBlockMethod(BanMethod):
         input_chain.insert_rule(custom_chain_redirect_rule)
 
 
+
+    def export_chain_rules_to_datastore(self):
+        for rule in self.custom_chain.rules:
+            self.ip_datastore.add(rule.src)
+
+    def add_ip_to_datastore(self, ip):
+        self.ip_datastore.add(ip)
+    
+    def remove_ip_from_datastore(self, ip):
+        self.ip_datastore.discard(ip)
+    
+    def is_ip_in_datastore(self, ip):
+        return ip in self.ip_datastore
+
+
     def ban(self, ip):
         """
         add drop rule with *ip*
         """
-        rule = iptc.Rule()
-        rule.src = ip
-        rule.create_target("DROP")
-        rule.final_check()
-        self.custom_chain.append_rule(rule)
+        if not self.is_ip_in_datastore(ip):
+            rule = iptc.Rule()
+            rule.src = ip
+            rule.create_target("DROP")
+            rule.final_check()
+            self.custom_chain.append_rule(rule)
+
+            self.add_ip_to_datastore(ip)
 
 
     def unban(self, ip):
         """
         delete drop rule with *ip*
         """
-        rule = iptc.Rule()
-        rule.src = ip
-        rule.create_target("DROP")
-        rule.final_check()
-        self.custom_chain.delete_rule(rule)
+        if self.is_ip_in_datastore(ip):
+            rule = iptc.Rule()
+            rule.src = ip
+            rule.create_target("DROP")
+            rule.final_check()
+            self.custom_chain.delete_rule(rule)
+
+            self.remove_ip_from_datastore(ip)
 
     def flush(self):
         """
@@ -65,7 +87,7 @@ class IptablesBlockMethod(BanMethod):
     
     def cleanup(self):
         """
-        move all chains and rules created by class
+        remove all chains and rules created by class
         """
         input_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
         custom_chain_rule = iptc.Rule()
@@ -76,4 +98,10 @@ class IptablesBlockMethod(BanMethod):
         self.custom_chain.delete()
 
 if __name__ == "__main__":
+    # export XTABLES_LIBDIR=/usr/lib/xtables
     a = IptablesBlockMethod()
+    a.cleanup()
+    # a.ban('192.168.1.120')
+    # a.ban('192.168.1.121')
+    # a.ban('192.168.1.122')
+    # a.ban('192.168.1.123')
